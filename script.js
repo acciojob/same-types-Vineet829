fa2f30c78c14d8738e42a9aa0663b08134d6ac96
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-13 16:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function isSameType(value1, value2) {
-    if(Number(value1) === NaN &&  Number(value2) === NaN){
-		 return true
-	}
-	else if(Number(value1) !== NaN && Number(value2) !== NaN && typeof Number(value1) === typeof Number(value2) {
-		 return true
-		}    
-	
-		else{
-		return false
-	}
+    const num1 = Number(value1);
+    const num2 = Number(value2);
+
+    if (isNaN(num1) && isNaN(num2)) {
+        return true;
+    }
+
+    if (typeof value1 === typeof value2 && !isNaN(num1) && !isNaN(num2)) {
+        return true;
+    }
+
+    return false;
 }
 
-// do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
